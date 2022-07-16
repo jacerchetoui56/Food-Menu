c55468fc51beb3d54754f4efc1e473c26ac29145
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-07-16 17:20:20 +0100

    just revising the code and cleaning it

--- src/Card.js
+++ src/Card.js
@@ -1,22 +1,22 @@
 import React from 'react'
 
-export default function Card(props) {
+export default function Card({ img, title, price, desc }) {
     return (
         <div className='food-card'>
             <div className='food-image'>
-                <img src={props.img} alt="" />
+                <img src={img} alt="" />
             </div>
             <div className="food-info">
                 <div className="food-title">
                     <h3 className='food-name'>
-                        {props.title}
+                        {title}
                     </h3>
                     <h3 className="food-price">
-                        ${props.price}
+                        ${price}
                     </h3>
                 </div>
                 <div className="food-desc">
-                    {props.desc}
+                    {desc}
                 </div>
             </div>
         </div>
